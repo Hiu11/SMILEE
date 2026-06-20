92c658e90a07cc0244d3cbfa61e7eff0517192a1
Hiu11 <Tronghieudo2k4@gmail.com>
2026-06-20 11:20:49 +0700

    Remove obsolete JavaScript files and images related to doctor management, records, reception, and reset functionalities. This includes the deletion of various scripts handling doctor and patient records, invoice management, dropdown functionality, and reset processes, as well as associated images used in the application.

--- js/reset.js
+++
@@ -1,6 +0,0 @@
-const resetForm = document.getElementById('resetForm');
-
-resetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    window.location.href = 'reset-otp.html';
-});
